e714c175f5d0afd0e2d76309c21c35c7085ead5e
David Benveniste <david.benveniste@creatis.insa-lyon.fr>
2021-01-29 11:28:20 +0100

    Update scatter correction script with more parameters

--- scatter_correction_dew.py
+++ scatter_correction_dew.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import numpy as np
 import click
@@ -8,38 +8,48 @@ import itk
 #------------------------------------------------------------------------------
 CONTEXT_SETTINGS = dict(help_options=['-h', '--help'])
 @click.command(context_settings=CONTEXT_SETTINGS)
-@click.option('--input','-i', 'input_file', help='Input mhd projection file')
-@click.option('--output','-o','output_file',help='Output mhd file with scatter corrected projection')
+@click.option('--input','-i', 'input_image', help='Input mhd projection file')
+@click.option('--head',default=2, help='Number of detectors')
+@click.option('--angles',help='Number of angles of one detector')
+@click.option('--primary_window','-pw',type=(int,int),help='Indexes of the primary window')
+@click.option('--scatter_window','-sw',type=(int,int),help='Indexes of the scatter window')
 
-def scatter_correction_dewClick(input_file, output_file):
+def scatter_correction_dewClick(input_image,head,angles,primary_window,scatter_window):
     '''
     Compute the scatter correction in a cas of a double energy window for mhd projection and save the result in a mhd file. The user need to specify the energy limits for the main window and for the scatter window.
     '''
-    scatter_correction_dew(input_file, output_file)
+    scatter_correction_dew(input_image,head,angles,primary_window,scatter_window)
 
-def scatter_correction_dew(input_file, output_file):
+def scatter_correction_dew(input_image,head,angles,primary_window,scatter_window):
 
-    image = itk.imread(input_file)
-    array = (itk.array_from_image(image)).astype(float)
-    nb_energy_window = 2
-    nb_energy = 2*nb_energy_window
-    nb_angle = int(array.shape[0] /(2*nb_energy))
+    array = (itk.array_from_image(input_image)).astype(float)
+    size = angles * len(primary_window)*len(scatter_window)*head
+    if size != array.shape[0]:
+        print('There is a difference beteween the calculated number of slice and the actual one')
+        return []
 
-    array_head1_energy1 = array[0:nb_angle,:,:] - 1*1*array[2*nb_angle:3*nb_angle,:,:]
-    array_head1_energy2 = array[4*nb_angle:5*nb_angle,:,:] - 1.1*array[6*nb_angle:7*nb_angle,:,:]
-    array_head2_energy1 = array[nb_angle:2*nb_angle,:,:] - 1*1*array[3*nb_angle:4*nb_angle,:,:]
-    array_head2_energy2 = array[5*nb_angle:6*nb_angle,:,:] - 1.1*array[7*nb_angle:8*nb_angle,:,:]
+    pw1 = primary_window[0]
+    pw2 = primary_window[1]
+    sw1 = scatter_window[0]
+    sw2 =  scatter_window[1]
+    array_res = []
+    
+    for i in range(head):
+        k = head*angles
+        array_energy1 = array[k*pw1:k*(pw1+1),:,:] - 1*1*array[sw1*k:(sw1+1)*k,:,:]
+        array_energy2 = array[k*pw2:k*(pw2+1),:,:] - 1.1*array[k*sw2:(sw2+1)*k,:,:]
+        array_energy1[array_energy1 < 0] = 0
+        array_energy2[array_energy2 < 0] = 0
+        array_res.append((array_energy1, array_energy2))
 
-    array_head1_energy1[array_head1_energy1 < 0] = 0
-    array_head1_energy2[array_head1_energy2 < 0] = 0
-    array_head2_energy1[array_head2_energy1 < 0] = 0
-    array_head2_energy2[array_head2_energy2 < 0] = 0
+    
 
-    array_head1 = np.concatenate((array_head1_energy1,array_head1_energy2))
-    array_head2 = np.concatenate((array_head2_energy1,array_head2_energy2))
-    itkimg= np.stack(array_head1 + array_head2)
-    itkimg = itk.image_from_array(itkimg)
-    itk.imwrite(itkimg, output_file)
+    #array_head1 = np.concatenate((array_head1_energy1,array_head1_energy2))
+    #array_head2 = np.concatenate((array_head2_energy1,array_head2_energy2))
+    #itkimg= np.stack(array_head1 + array_head2)
+    #itkimg = itk.image_from_array(itkimg)
+    
+    #return itkimg
 
 
 # -----------------------------------------------------------------------------
@@ -61,4 +71,7 @@ class Test_scatter_correction_dew_(unittest.TestCase):
         filenameRaw = wget.download("https://gitlab.in2p3.fr/OpenSyd/syd_tests/-/raw/master/dataTest/fantome_sans_correction.raw?inline=false", out=tmpdirpath, bar=None)
 
         output = os.path.join(tmpdirpath, 'im_corrected.mhd')
-        scatter_correction_dew(filenameMhd, output)
+        img = itk.imread(filenameMhd)
+        res = scatter_correction_dew(img, 2,15,[0,2],[1,3])
+        self.assertTrue(res != [])
+        shutil.rmtree(tmpdirpath)
